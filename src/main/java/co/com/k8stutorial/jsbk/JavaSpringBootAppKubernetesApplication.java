adb11cfdb248acbc3a02797015e41943a5431ef9
Andres Garcia <agarcia@starbucks.com>
2022-04-06 15:17:02 -0500

    initial commit project creation

---
+++ src/main/java/co/com/k8stutorial/jsbk/JavaSpringBootAppKubernetesApplication.java
@@ -0,0 +1,13 @@
+package co.com.k8stutorial.jsbk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootAppKubernetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootAppKubernetesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/k8stutorial/jsbk/JavaSpringBootAppKubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.k8stutorial.jsbk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootAppKubernetesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
